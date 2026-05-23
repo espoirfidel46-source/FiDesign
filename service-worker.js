ca7eba2c6c7f71e732fa589dac7bddac6a445297
espoirfidel46-source <espoirfidel46@gmail.com>
2026-05-23 12:56:25 +0200

    Transformation FiDesign en application PWA

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "fidesign-cache-v1";
+
+const urlsToCache = [
+
+"/",
+"/index.html",
+"/dashboard.html",
+"/assistant.html",
+"/course.html",
+"/profile.html",
+"/certificate.html"
+
+];
+
+self.addEventListener("install",(event)=>{
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then((cache)=>{
+
+return cache.addAll(urlsToCache);
+
+})
+
+);
+
+});
+
+self.addEventListener("fetch",(event)=>{
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then((response)=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
